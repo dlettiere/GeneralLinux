970457e9c005cd34e128ed46deb1501bfc8793d1
dlettiere <dlettiere+git@gmail.com>
2025-03-08 09:43:45 -0500

    Update and rename getba.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 rm ~/.bash_aliases
 wget -P ~/ https://raw.githubusercontent.com/dlettiere/GeneralLinux/refs/heads/main/.bash_aliases
+sudo apt update
+sudo apt install tmux btop htop nmap tilde qdirstat 
 exit
