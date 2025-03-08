a35931f380d6d4d56d2139427f6254b53f6477c7
dlettiere <dlettiere+git@gmail.com>
2025-03-08 09:52:13 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 rm ~/.bash_aliases
 wget -P ~/ https://raw.githubusercontent.com/dlettiere/GeneralLinux/refs/heads/main/.bash_aliases
 sudo apt update
-sudo apt install tmux btop htop nmap tilde qdirstat rdiff-backup openssh rsync git
+sudo apt install tmux btop htop nmap tilde qdirstat rdiff-backup rsync git
 exit
