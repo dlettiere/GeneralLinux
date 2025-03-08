40cda762bb348a11d24b3c6d81e3f58cddb91c39
dlettiere <dlettiere+git@gmail.com>
2025-03-08 10:10:43 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,7 @@ rm ~/.bash_aliases
 wget -P ~/ https://raw.githubusercontent.com/dlettiere/GeneralLinux/refs/heads/main/.bash_aliases
 sudo apt update
 sudo apt install -y tmux btop htop nmap tilde qdirstat rdiff-backup rsync git
+curl -fsSL https://tailscale.com/install.sh | sh
+curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh | sh
+sudo usermod -aG nordvpn $USER
 exit
