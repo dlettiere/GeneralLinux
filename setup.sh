02c43759fa775470b4c867b66f0fea74e4a8161b
dlettiere <dlettiere+git@gmail.com>
2025-09-29 10:49:53 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,16 @@ rm ~/.bash_aliases
 wget -P ~/ https://raw.githubusercontent.com/dlettiere/GeneralLinux/refs/heads/main/.bash_aliases
 touch ~/.bash_aliases_local
 
+###Base Packages to install
+basepack="tmux btop htop nmap tilde qdirstat rdiff-backup rsync git vnstat tldr curl"
+
 printf 'Install base components (y/n)? '
+printf '$basepack'
 old_stty_cfg=$(stty -g)
 stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_cfg # Careful playing with stty
 if [ "$answer" != "${answer#[Yy]}" ];then
     sudo apt update
-    sudo apt install -y tmux btop htop nmap tilde qdirstat rdiff-backup rsync git vnstat tldr curl
+    #sudo apt install -y tmux btop htop nmap tilde qdirstat rdiff-backup rsync git vnstat tldr curl
 else
     echo ok
 fi
